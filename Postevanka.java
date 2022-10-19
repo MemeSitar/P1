afd1c1761fa8159044e066ae50af3ff51c835849
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-10-19 19:51:40 +0000

    Postevanka.

--- Postevanka.java
+++ Postevanka.java
@@ -7,7 +7,7 @@ public class Postevanka{
         int stevilka = sc.nextInt();
         for (int stevec1 = 1; stevec1 <= stevilka; stevec1++){
             for (int stevec2 = 1; stevec2 <= stevilka; stevec2++){
-                System.out.print(stevec1*stevec2 + " ");
+                System.out.printf("%4d",stevec1*stevec2);
             }
             System.out.println();
         }
