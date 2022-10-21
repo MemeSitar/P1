b3411256f30234a72e1051c279b2957ec3b3ba78
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-10-21 10:28:29 +0000

    prva verzija

--- domace_naloge/DN01/DN01_63220294.java
+++ domace_naloge/DN01/DN01_63220294.java
@@ -1,20 +1,29 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class DN01_63220294{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+
+        int stKvadratov = 0;
         int stranicaA = sc.nextInt();
         int stranicaB = sc.nextInt();
 
         // obrne stranici, ce je stranicaA vecja od stranicaB
         // pravokotnik je vedno vodoraven
+        // stranicaB je vodoravna stranica, vedno vecja.
         if (stranicaA > stranicaB){
             int temp = stranicaA;
             stranicaA = stranicaB;
             stranicaB = temp;
         }
 
-        
+        stranicaA -= 1;
+        stranicaB -= 1;
+        while (stranicaB >= 1){
+            stKvadratov = stranicaA * stranicaB;
+            stranicaA -= 1;
+            stranicaB -= 1;
+        }
+        System.out.println(stKvadratov);
     }
 }
