e5c9e99f6d0ac84a7e69cdb44e7bd1c68d37582c
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-10-30 13:28:14 +0100

    Raznovrstnica dela.

--- domace_naloge/DN02/DN02_63220294.java
+++ domace_naloge/DN02/DN02_63220294.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 public class DN02_63220294{
+    // scanner prestavljen ven da je dostopen vsem metodam.
+    static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args){
         // setup
-        Scanner sc = new Scanner(System.in);
         int rezultat = 0;
 
         // osnovni vnosi.
@@ -30,13 +32,12 @@ public class DN02_63220294{
     }
 
     public static int Raznovrstnica(int dolzina, int stVnos){
-        Scanner sc = new Scanner(System.in);
         int rezultat = 0;
         int vnos = 0;
         int buffer = -1;
 
         // glavni loop
-        for (int i = 0; i < stVnos; i++){
+        for (int i = 1; i < stVnos; i++){
             // ce se vnos izvede prvic
             if (buffer == -1){
                 buffer = sc.nextInt();
@@ -50,19 +51,16 @@ public class DN02_63220294{
     }
 
     public static int Kvadratnica(int stranica){
-        Scanner sc = new Scanner(System.in);
         int rezultat = 0;
         return rezultat;
     }
 
     public static int Piramidnica(int visina){
-        Scanner sc = new Scanner(System.in);
         int rezultat = 0;
         return rezultat;
     }
 
     public static int Spiralnica(int stranica){
-        Scanner sc = new Scanner(System.in);
         int rezultat = 0;
         return rezultat;
     }
