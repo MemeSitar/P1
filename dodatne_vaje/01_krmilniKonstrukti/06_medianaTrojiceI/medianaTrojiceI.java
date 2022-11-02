e4009b350914d8ad68b4116a57719f512a533d29
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-11-02 11:37:03 +0100

    medianaTrojice dela.

--- dodatne_vaje/01_krmilniKonstrukti/06_medianaTrojiceI/medianaTrojiceI.java
+++ dodatne_vaje/01_krmilniKonstrukti/06_medianaTrojiceI/medianaTrojiceI.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 public class medianaTrojiceI{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        int rezultat = 0;
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if (a >= b && b >= c){
-            
+        if (a >= b && b >= c || a <= b && b <= c){
+            rezultat = b;
+        } else if (a >= c && c >= b || a <= c && c <= b){
+            rezultat = c;
+        } else {
+            rezultat = a;
         }
-        System.out.println(d);
+        System.out.println(rezultat);
     }
 }
