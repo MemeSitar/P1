45f43c9c7b8fe32da5f6aab9f1e6acd2de9de659
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-11-16 09:33:58 +0100

    dodal vajo 6

---
+++ domace_naloge/DN04/DN04_63220294.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DN04_63220294{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int povabljeni = sc.nextInt();
+        int srecnoStevilo = sc.nextInt();
+        int[] stevilke = new int[povabljeni];
+        for (int i = 0; i < povabljeni; i++){
+            stevilke[i] = sc.nextInt();
+        }
+        
+    }
+}
