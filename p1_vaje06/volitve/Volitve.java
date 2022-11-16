e1e27e105cb871b64eed78f3dd959b85938cb057
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-11-16 10:01:13 +0100

    Prvi trije primeri delajo!

--- p1_vaje06/volitve/Volitve.java
+++ p1_vaje06/volitve/Volitve.java
@@ -6,18 +6,39 @@ public class Volitve {
     }
 
     public static int steviloGlasov(int[][][] t, int leto, int stranka) {
-        // popravite / dopolnite
-        return -1;
+        int sestevek = 0;
+        for (int volisce = 0; volisce < t[leto][stranka].length; volisce++){
+            sestevek += t[leto][stranka][volisce];
+        }
+        return sestevek;
     }
 
     public static int[][] glasovi(int[][][] t) {
-        // popravite / dopolnite
-        return null;
+        int[][] rezultati = new int[t.length][t[0].length];
+        for (int leto = 0; leto < t.length; leto++){
+            for (int stranka = 0; stranka < t[0].length; stranka++){
+                rezultati[leto][stranka] = steviloGlasov(t, leto, stranka);
+            }
+        }
+        return rezultati;
     }
 
     public static int najVolisce(int[][][] t, int stranka) {
-        // popravite / dopolnite
-        return -1;
+        int topRezultat = 0;
+        int topVolisce = 0;
+        int[] voliscaSestevek = new int[t[0][stranka].length];
+        for (int leto = 0; leto < t.length; leto++){
+            for (int volisce = 0; volisce < t[0][stranka].length; volisce++){
+                voliscaSestevek[volisce] += t[leto][stranka][volisce];
+            }
+        }
+        for (int volisce = 0; volisce < voliscaSestevek.length; volisce++){
+            if (voliscaSestevek[volisce] > topRezultat){
+                topRezultat = voliscaSestevek[volisce];
+                topVolisce = volisce;
+            }
+        }
+        return topVolisce;
     }
 
     public static int vsotaUvrstitev(int[][][] t, int stranka, int volisce) {
