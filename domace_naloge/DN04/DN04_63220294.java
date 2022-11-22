5ba88c575bda7c4f65a0695b76dce12bac3d8b69
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-11-22 13:59:04 +0100

    dodal nekaj malega k nalogi.

--- domace_naloge/DN04/DN04_63220294.java
+++ domace_naloge/DN04/DN04_63220294.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class DN04_63220294{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        
+        // osnovni vnosi
+        long rezultat = 0;
         int povabljeni = sc.nextInt();
         int srecnoStevilo = sc.nextInt();
+
+        // ustvari tabelco
         int[] stevilke = new int[povabljeni];
         for (int i = 0; i < povabljeni; i++){
             stevilke[i] = sc.nextInt();
         }
+        // algoritem tukaj
         
+        // rezultat
+        System.out.println(rezultat);
     }
 }
