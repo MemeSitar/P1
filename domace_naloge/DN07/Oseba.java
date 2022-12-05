1764f33310f0d6108a068f83c63eb5b5d2cf8536
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-12-05 15:45:33 +0100

    Dodal datoteke za dn7

---
+++ domace_naloge/DN07/Oseba.java
@@ -0,0 +1,17 @@
+
+public class Oseba {
+
+    public Oseba(String ip, char spol, int starost) {
+        // dopolnite ...
+    }
+
+    public String toString() {
+        // dopolnite/popravite ...
+        return null;
+    }
+
+    public boolean jeStarejsaOd(Oseba os) {
+        // dopolnite/popravite ...
+        return false;
+    }
+}
---
+++ domace_naloge/DN07/Stanovanje.java
@@ -0,0 +1,42 @@
+
+public class Stanovanje {
+
+    public Stanovanje(Oseba[] stanovalci) {
+        // dopolnite ...
+    }
+
+    public int steviloStanovalcev() {
+        // dopolnite/popravite ...
+        return 0;
+    }
+
+    public int steviloStarejsihOd(Oseba os) {
+        // dopolnite/popravite ...
+        return 0;
+    }
+
+    public int[] mz() {
+        // dopolnite/popravite ...
+        return null;
+    }
+
+    public Oseba starosta() {
+        // dopolnite/popravite ...
+        return null;
+    }
+
+    public void nastaviSosede(Stanovanje levi, Stanovanje zgornji,
+            Stanovanje desni, Stanovanje spodnji) {
+        // dopolnite ...
+    }
+
+    public Oseba starostaSosescine() {
+        // dopolnite/popravite ...
+        return null;
+    }
+
+    public Oseba[] sosedjeSosedov() {
+        // dopolnite/popravite ...
+        return null;
+    }
+}
