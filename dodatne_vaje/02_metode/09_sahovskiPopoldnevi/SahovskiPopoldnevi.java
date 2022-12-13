e23063acf2ca17262efbef8c84a3f92fa1085294
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-12-13 18:35:07 +0100

    se dve funkciji!

--- dodatne_vaje/02_metode/09_sahovskiPopoldnevi/SahovskiPopoldnevi.java
+++ dodatne_vaje/02_metode/09_sahovskiPopoldnevi/SahovskiPopoldnevi.java
@@ -13,6 +13,34 @@ public class SahovskiPopoldnevi{
         int stZmag = sc.nextInt();
         int stDni = sc.nextInt();
 
+        for (int i = 1; i <= stDni; i++){
+            System.out.printf("%d. dan: ");
+            simulirajDan(verA, verB, rand);
+        }
+    }
+
+    public static void simulirajDan(int verA, int verB, Random rand, int stZmag, int stPartij){
+        int zmageA = 0;
+        int zmageB = 0;
+        int i = 0;
+        char rezultat = ''
+
+        while (Math.max(zmageA, zmageB) <= stZmag && i < stPartij){
+            rezultat = simulirajPartijo(verA, verB, rand);
+            switch (rezultat){
+                case 'A' :
+                    zmageA++;
+                    break;
+                case 'B'
+                    zmageB++;
+                    break;
+                case 'r'}
+            i++;
+        }
+        System.out.printf(" (%d)\n", i);
+    }
+
+    public static char simulirajPartijo(int verA, int verB, Random rand){
         
     }
 }
