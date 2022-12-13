ba7fb056dc1bfb46c2c5fb33e587d33c6ad89bad
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-12-13 12:46:49 +0100

    Zacetek postavljen.

---
+++ dodatne_vaje/02_metode/09_sahovskiPopoldnevi/SahovskiPopoldnevi.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class SahovskiPopoldnevi{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int seme = sc.nextInt();
+        Random rand = new Random(seme);
+
+        int verA = sc.nextInt();
+        int verB = sc.nextInt();
+        int stPartij = sc.nextInt();
+        int stZmag = sc.nextInt();
+        int stDni = sc.nextInt();
+
+        
+    }
+}
