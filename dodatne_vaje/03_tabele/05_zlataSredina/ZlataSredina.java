51f4636c62acf0df78239e835801277717506a4c
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-12-14 20:10:16 +0100

    Moj prvi sortirni algoritem!!!

--- dodatne_vaje/03_tabele/05_zlataSredina/ZlataSredina.java
+++ dodatne_vaje/03_tabele/05_zlataSredina/ZlataSredina.java
@@ -4,16 +4,35 @@ public class ZlataSredina{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int k = scanner.nextInt();
-        int[] t = new int[2*k+1];
-        ustvariTabelo(sc, 2*k+1, t);
+        int k = sc.nextInt();
+        int stElementov = 2 * k + 1;
+        int[] t = new int[stElementov];
+        ustvariTabelo(sc, stElementov, t);
 
-        
+        int[] urejenaTabela = urediTabelo(t);
+        System.out.println(urejenaTabela[k]);
     }
 
     public static void ustvariTabelo(Scanner sc, int n, int[] t){
         for (int i = 0; i < n; i++){
             t[i] = sc.nextInt();
         }
+    }
+
+    public static int[] urediTabelo(int[] t){
+        int[] rezultat = new int[t.length];
+        int najmanjsaCifra = (int) Integer.MAX_VALUE;
+        int trenutnaCifra = (int) Integer.MIN_VALUE;
+        for (int i = 0; i < rezultat.length; i++){
+            najmanjsaCifra = (int) Integer.MAX_VALUE;
+            for (int j = 0; j < t.length; j++){
+                if (t[j] < najmanjsaCifra && t[j] > trenutnaCifra){
+                    najmanjsaCifra = t[j];
+                }
+            }
+            rezultat[i] = najmanjsaCifra;
+            trenutnaCifra = najmanjsaCifra;
+        }
+        return rezultat;
     }
 }
