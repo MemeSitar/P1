fa6e21832406cc377fe92ad2049b896a25db3272
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-12-14 16:27:07 +0100

    YAY!

--- dodatne_vaje/03_tabele/04_telefonskiImenik/TelefonskiImenik.java
+++ dodatne_vaje/03_tabele/04_telefonskiImenik/TelefonskiImenik.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class TelefonskiImenik{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        sc.nextLine();
         String[][] t = new String[n][2];
         ustvariTabelo(sc, n, t);
-
+        
         int k = sc.nextInt();
-        String niz = ""
+        sc.nextLine();
+        String niz = "";
         // glavni loop
         for (int i = 0; i < k; i++){
-            niz = sc.nextString();
+            niz = sc.nextLine();
             System.out.println(poisciVTabeli(niz, t));
         }
     }
 
     public static void ustvariTabelo(Scanner sc, int n, String[][] t){
         for (int i = 0; i < n; i++){
-            for (int j = 0; j < 2; j++){
-                t[i][j] = sc.nextString();
-            }
-
+            t[i] = sc.nextLine().split(" ");
+            //System.out.println(Arrays.toString(t[i]));
+            //System.out.println(i);
         }
     }
 
     public static String poisciVTabeli(String niz, String[][] t){
-        for (int i = 0; i < t.length; i++){
+        for (int i = t.length - 1; i >= 0; i--){
             if (t[i][0].equals(niz)){
                 return t[i][1];
             }
