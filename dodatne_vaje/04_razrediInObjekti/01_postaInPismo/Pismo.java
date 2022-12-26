d86fd88382f9918f9eb01c177177764999d4af4b
Sitar <65349137+MemeSitar@users.noreply.github.com>
2022-12-26 17:37:01 +0100

    Vse skp dela!

--- dodatne_vaje/04_razrediInObjekti/01_postaInPismo/Pismo.java
+++ dodatne_vaje/04_razrediInObjekti/01_postaInPismo/Pismo.java
@@ -3,6 +3,9 @@ public class Pismo{
     private Posta ciljna;
     private boolean jePriporoceno;
     private int razdalja;
+    private static int r;
+    private static int c;
+    private static int p;
 
     public Pismo(Posta izvorna, Posta ciljna, boolean jePriporoceno, int razdalja){
         this.izvorna = izvorna;
@@ -13,9 +16,55 @@ public class Pismo{
 
     public String toString(){
         char vrsta = 'N';
-        if (jePriporoceno){
+        if (this.jePriporoceno){
             vrsta = 'P';
         }
         return String.format("%s -> %s (%d km) [%c]", izvorna.toString(), ciljna.toString(), this.razdalja, vrsta);
+    }
+
+    public boolean izviraOd(Posta posta){
+        return this.izvorna.equals(posta);
+    }
+
+    public boolean staIzvorInCiljIsta(){
+        return this.izvorna.equals(this.ciljna);
+    }
+
+    public boolean imaIstiCiljKot(Pismo pismo){
+        return this.ciljna.equals(pismo.ciljna);
+    }
+
+    public static boolean imataIstiCilj(Pismo p1, Pismo p2){
+        return p1.ciljna.equals(p2.ciljna);
+    }
+
+    public int cena(){
+        int cena = 0;
+        cena = ((this.razdalja / (this.r)) + 1)*this.c;
+        if (this.jePriporoceno){
+            cena += this.p;
+        }
+        return cena;
+    }
+
+    public static void nastaviKonstanteZaCeno(int enotaRazdalje, int enotaCene, int priporocnina){
+        r = enotaRazdalje;
+        c = enotaCene;
+        p = priporocnina;
+    }
+
+    public boolean jeDrazjeOd(Pismo pismo){
+        return (this.cena() > pismo.cena());
+    }
+
+    public static Pismo vrniDrazje(Pismo p1, Pismo p2){
+        if (p1.cena() > p2.cena()){
+            return p1;
+        }
+        return p2;
+    }
+
+    public Pismo izdelajPovratno(){
+        return new Pismo(this.ciljna, this.izvorna, this.jePriporoceno, this.razdalja);
     }
 }
